c058f3b5cabc46bf8855c0add75ceb943f4e3d5a
SagarSabat <sagarssabat@gmail.com>
2020-04-26 16:24:47 +0530

    calculator html with basic styling

---
+++ javascript/clickevent.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function getDetails() {
+    var username = document.querySelector("#username").value;
+    var userpassword = document.querySelector("#userpassword").value;
+
+    if (username == "") {
+        document.querySelector(".errormsg").no
+    }
+}
